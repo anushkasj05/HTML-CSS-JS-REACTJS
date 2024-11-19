e3106a2ea06d19bf83267f2ad07e2ee858281188
anushka <anushka.sj05@gmail.com>
2024-11-19 18:51:16 +0530

    first commit

---
+++ 16 Nov/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  //   $(".slide").click(function () {
+  //     $(".content").slideDown();
+  //   });
+  //   $(".slide").click(function () {
+  //     $(".content").slideUp();
+  //   });
+  $(".slide").click(function () {
+    $(".content").slideToggle();
+  });
+});
---
+++ 18 Nov/script.js
@@ -0,0 +1,5 @@
+/*
+React is a javascript library created by facebook to create user interface(UI).
+React is used to make a single page application(spa).
+React allows us to create reusable UI components.
+*/
